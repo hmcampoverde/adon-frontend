33d3ba6c4f32c3f549d8be1e5b034be621636272
Henry Manuel Campoverde Hidalgo <hmcampoverde@gmail.com>
2024-06-24 15:52:04 -0500

    Agregar componente Confirm Dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [NgxSpinnerModule, ToastModule, RouterOutlet],
+	imports: [ConfirmDialogModule, NgxSpinnerModule, ToastModule, RouterOutlet],
 	templateUrl: './app.component.html'
 })
 export class AppComponent {}
