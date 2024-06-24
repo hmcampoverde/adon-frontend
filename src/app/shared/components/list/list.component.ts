506b3a1e06bda04d9f64ab124e989e87ac9a6130
Henry Manuel Campoverde Hidalgo <hmcampoverde@gmail.com>
2024-06-24 15:49:59 -0500

    Creación de componente general para listar los registros

---
+++ src/app/shared/components/list/list.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { DefaultService } from '../../../core/services/default.service';
+
+@Component({
+	selector: 'app-list',
+	standalone: true,
+	imports: [],
+	template: ``
+})
+export class ListComponent<T> implements OnInit {
+	protected settings: any;
+	protected route: string = '/';
+	protected items: Array<T> = [];
+	protected item: any;
+
+	protected router = inject(Router);
+	protected service: DefaultService<T>;
+
+	protected confirmationService = inject(ConfirmationService);
+	protected messageService = inject(MessageService);
+
+	ngOnInit(): void {
+		this.service.findAll().subscribe((items) => (this.items = items));
+	}
+
+	protected rowClick(data: any) {
+		this.item = data.item;
+		switch (data.action) {
+			case 'create':
+				this.create();
+				break;
+			case 'update':
+				this.update();
+				break;
+			case 'remove':
+				this.remove();
+				break;
+		}
+	}
+
+	protected create(): void {
+		this.router.navigate([`${this.route}/create`]);
+	}
+
+	protected update(): void {
+		if (this.item) {
+			this.router.navigate([`${this.route}/update/${this.item.id}`]);
+		}
+	}
+
+	protected remove(): void {
+		this.confirmationService.confirm({
+			message: 'Desea eliminar el registro seleccionado?',
+			header: 'Confirmación de Eliminación',
+			icon: 'pi pi-exclamation-triangle',
+			accept: () => {
+				this.service.deleteById(this.item.id).subscribe({
+					next: (response) => {
+						this.messageService.add(response);
+						this.items = this.items.filter((item: any) => item.id !== this.item.id);
+					}
+				});
+			}
+		});
+	}
+}
